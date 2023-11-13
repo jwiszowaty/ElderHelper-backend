70e10aae3f1b54f6ab3b8c8d66a92876ac9dffe0
Stanley <27.stanleylch@gmail.com>
2023-11-13 14:04:58 +0000

    socket_chat 1 and fetchUser fix

--- listen.js
+++ listen.js
@@ -1,6 +1,10 @@
 const { app, server } = require("./app.js");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}...`);
+// });
+
+server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
 });
